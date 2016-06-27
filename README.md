9ef2c2db6007a83c2ddae50b949af2b108afcfc6
Seena <seenarowhani95@gmail.com>
2016-06-27 11:45:43 -0400

    set up so that promise chain bubbles

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ember-cli-deploy-init
+# ember-cli-deploy-frost
 
 This README outlines the details of collaborating on this Ember addon.
 
--- index.js
+++ index.js
@@ -1,42 +1,15 @@
 /* jshint node: true */
 (function(Promise, exec, path, git) {
-  'use strict';
+  'use strict'
   module.exports = {
-    name: 'ember-cli-deploy-init',
+    name: 'ember-cli-deploy-frost',
     createDeployPlugin: function(options) {
       let currentBranch = null
       let branch = options.branch || 'gh-pages'
-      let create = `git checkout --orphan ${branch}; git commit -m --allow-empty; git push -u origin ${branch}`
+      let create = `git checkout --orphan ${branch} git commit -m --allow-empty git push -u origin ${branch}`
       return {
         name: options.name,
-        configure(context) {
-          var pluginConfig = context.config[this.name] || {};
-          return getMyRepo(context).then(function(myRepo) {
-            return {
-              gitDeploy: {
-                myRepo: myRepo,
-                repo: pluginConfig.repo || myRepo,
-                branch: pluginConfig.branch || 'gh-pages',
-                worktreePath: pluginConfig.worktreePath || defaultWorktree(context)
-              }
-            };
-          }).catch(showStderr(context.ui));
-        },
-        upload(context) {
-          var d = context.gitDeploy;
-          var distDir = context.distDir || path.join(context.project.root, 'dist');
-          return git.prepareTree(d.worktreePath, d.myRepo, d.repo, d.branch)
-            .then(function() {
-              return git.replaceTree(d.worktreePath, distDir);
-            }).then(function(didCommit) {
-              if (didCommit) {
-                return git.push(d.worktreePath, d.repo, d.branch);
-              } else {
-                console.log("Nothing to deploy");
-              }
-            }).catch(showStderr(context.ui));
-        },
-        setup() {
+        setup () {
           return new Promise((resolve, reject) => {
             console.log(`Checking if branch '${branch}' already exists...`)
             exec(`git name-rev --name-only HEAD`, (err, out) => {
@@ -54,34 +27,48 @@
             })
           })
         },
-        teardown() {
+        configure(context) {
           return new Promise((resolve, reject) => {
-            if (oldBranch)
-              exec(`git checkout ${oldBranch}`, (err, out) => {
-                err ? reject(err) : resolve(out)
-              })
+            context.ui.verbose = true
+            var pluginConfig = context.config[this.name] || {}
+            return git.origin(context.project.root).then(function(myRepo) {
+              let worktreePath = pluginConfig.worktreePath || path.join(context.project.root, '../deploy-' + context.project.name())
+              try {
+                exec(`rm -rf ${worktreePath}`, (err, out) => {
+                  err ? reject(err) : resolve(out)
+                })
+              } catch (e) {
+                reject(e)
+              }
+              return {
+                gitDeploy: {
+                  myRepo,
+                  repo: pluginConfig.repo || myRepo,
+                  branch: pluginConfig.branch || 'gh-pages',
+                  worktreePath
+                }
+              }
+            }).then(resolve).catch(reject)
           })
-
+        },
+        upload(context) {
+          return new Promise((resolve, reject) => {
+            var d = context.gitDeploy
+            var distDir = context.distDir || path.join(context.project.root, 'dist')
+            return git.prepareTree(d.worktreePath, d.myRepo, d.repo, d.branch)
+              .then(function() {
+                return git.replaceTree(d.worktreePath, distDir)
+              }).then(function(commit) {
+                if (commit) {
+                  return git.push(d.worktreePath, d.repo, d.branch)
+                } else {
+                  console.log("Nothing to deploy")
+                }
+              }).then(resolve).catch(reject)
+          })
         }
       }
     }
-  };
-
-  function showStderr(ui) {
-    return function(err) {
-      if (err.stderr) {
-        ui.write(err.stderr);
-      }
-      throw err;
-    };
-  }
-
-  function getMyRepo(context) {
-    return git.origin(context.project.root);
-  }
-
-  function defaultWorktree(context) {
-    return path.join(context.project.root, '../deploy-' + context.project.name());
   }
 })
 (
