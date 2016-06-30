800693506b0c5614ced4b780ddc3e5693d6a7682
Seena <seenarowhani95@gmail.com>
2016-06-30 11:39:13 -0400

    slack integration

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
 /* jshint node: true */
-(function(Promise, exec, path, git) {
+(function(Promise, exec, path, git, Slack) {
   'use strict'
   var DeployPluginBase = require('ember-cli-deploy-plugin');
   module.exports = {
     name: 'ember-cli-deploy-gh-pages',
     createDeployPlugin: function(options) {
+      let slack = null
+      let pluginConfig = {}
       let Plugin = DeployPluginBase.extend({
         name: options.name,
-        setup (context) {
-          let pluginConfig = context.config[this.name] || {}
-          let branch = pluginConfig.branch || 'gh-pages'
-          let commitMessage = pluginConfig.commitMessage || 'Deploy'
-
-          let create = `git checkout --orphan ${branch}; git commit -m "${commitMessage}"; git push -u origin ${branch}`
-          return new Promise((resolve, reject) => {
-            this.log(`Checking if branch '${branch}' already exists...`, {color:'yellow'})
-            exec(`git name-rev --name-only HEAD`, (err, currentBranch) => {
-              if (err) reject(err)
-
-              exec(`git branch | grep ${branch}`, (err, out) => {
-                if (err) {
-                  this.log(`Creating branch '${branch} ...'`, {color: 'yellow'})
-                  exec(`${create}; git checkout ${currentBranch}`, (err, out) => {
-                    err ? reject(err) : resolve(out)
-                  })
-                }
-                resolve(out)
-              })
-            })
-          })
-        },
         configure(context) {
           return new Promise((resolve, reject) => {
-            var pluginConfig = context.config[this.name] || {}
-            let branch = pluginConfig.branch || 'gh-pages'
-            let commitMessage = pluginConfig.commitMessage || 'Deploy'
+            pluginConfig = context.config[this.name] || {}
+            pluginConfig.branch = pluginConfig.branch || 'gh-pages'
+            pluginConfig.commitMessage = pluginConfig.commitMessage || 'Deploy'
+            if (pluginConfig.slack && pluginConfig.slack.webhookURL) {
+              slack = new Slack(pluginConfig.slack.webhookURL, pluginConfig.slack.options)
+            }
+            let branch = pluginConfig.branch
+            let commitMessage = pluginConfig.commitMessage
             context.ui.verbose = pluginConfig.verbose
             return git.origin(context.project.root).then(function(myRepo) {
               let worktreePath = pluginConfig.worktreePath || path.join(context.project.root, `../deploy-${context.project.name()}`)
@@ -54,22 +38,84 @@
             }).then(resolve).catch(reject)
           })
         },
+        setup(context) {
+          let create = `git checkout --orphan ${pluginConfig.branch}; git commit -m "${pluginConfig.commitMessage}"; git push -u origin ${pluginConfig.branch}`
+          return new Promise((resolve, reject) => {
+            this.log(`Checking if branch '${pluginConfig.branch}' already exists...`, {
+              color: 'yellow'
+            })
+            exec(`git name-rev --name-only HEAD`, (err, currentBranch) => {
+              if (err) reject(err)
+
+              exec(`git branch | grep ${pluginConfig.branch}`, (err, out) => {
+                if (err) {
+                  this.log(`Creating branch '${pluginConfig.branch} ...'`, {
+                    color: 'yellow'
+                  })
+                  exec(`${create}; git checkout ${currentBranch}`, (err, out) => {
+                    err ? reject(err) : resolve(out)
+                  })
+                }
+                resolve(out)
+              })
+            })
+          })
+        },
         upload(context) {
           var pluginConfig = context.config[this.name] || {}
-
-          return new Promise((resolve, reject) => {
-            var d = context.gitDeploy
-            var distDir = context.distDir || path.join(context.project.root, 'dist')
-            return git.prepareTree(d.worktreePath, d.myRepo, d.repo, d.branch)
-              .then(() => {
-                return git.replaceTree(d.worktreePath, distDir, d.commitMessage).catch(reject)
-              }).then((commit) => {
-                if (commit) {
-                  if (d.force)
-                    this.log(`Force pushing to ${d.branch}`, {color: 'yellow'})
-                  return git.push(d.worktreePath, d.repo, d.branch, d.force).catch(reject)
-                }
-              }).then(resolve).catch(reject)
+          return new Promise((res, rej) => {
+            return new Promise((resolve, reject) => {
+              var d = context.gitDeploy
+              var distDir = context.distDir || path.join(context.project.root, 'dist')
+              let s = pluginConfig.slack || {}
+              return git.prepareTree(d.worktreePath, d.myRepo, d.repo, d.branch)
+                .then(() => {
+                  return git.replaceTree(d.worktreePath, distDir, d.commitMessage).catch(reject)
+                }).then((commit) => {
+                  if (commit) {
+                    if (d.force)
+                      this.log(`Force pushing to ${d.branch}`, {
+                        color: 'yellow'
+                      })
+                    return git.push(d.worktreePath, d.repo, d.branch, d.force).catch(reject)
+                  }
+                }).then(resolve).catch(reject)
+            }).then((out) => {
+              slack.notify({
+                attachments: [{
+                  "fallback": "Success",
+                  "pretext": "Successful build!",
+                  "color": "good",
+                  "fields": [{
+                    "title": "Build completed.",
+                    "value": s.successMessage || `Succesfully built to branch ${d.branch}`,
+                    "short": false
+                  }]
+                }]
+              }, () => {
+                res(out)
+              })
+            }).catch((err) => {
+              slack.notify({
+                mrkdwn: true,
+                attachments: [{
+                  "fallback": "Deployment failed!",
+                  "pretext": "Deployment failed!",
+                  "color": "danger",
+                  "fields": [{
+                    "title": "Failure",
+                    "value": `Unable to deploy.`,
+                    "short": false
+                  }]
+                }, {
+                  "color": "danger",
+                  "text": `\`\`\`${err}\`\`\``,
+                  "mrkdwn_in": ["text"]
+                }]
+              }, () => {
+                rej(err)
+              })
+            })
           })
         }
       })
@@ -81,5 +127,6 @@
   require('ember-cli/lib/ext/promise'),
   require('child_process').exec,
   require('path'),
-  require('./lib/git')
+  require('./lib/git'),
+  require('node-slackr')
 )
